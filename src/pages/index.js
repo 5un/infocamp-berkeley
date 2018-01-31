a65eb9390c31565f91d42417a36ebd979233a984
Soravis Prakkamakul <asunnotthesun@gmail.com>
2018-01-31 15:22:01 -0800

    Added subscription form html

--- src/pages/index.js
+++ src/pages/index.js
@@ -24,6 +24,19 @@ const IndexPage = () => (
           <h1 style={{ textShadow: '0px 2px 10px rgba(43,255,255,0.5)' }}>Augmenting Society</h1>
           <h2 style={{ textShadow: '0px 2px 10px rgba(43,255,255,0.2)' }}>March 17th, 2018 at South Hall</h2>
           <span style={{ textShadow: '0px 2px 10px rgba(43,255,255,0.2)' }}>More Information Coming Soon...</span>
+          
+          <form action="https://infocamp.us2.list-manage.com/subscribe/post?u=536179e4b2849e96e841012a4&amp;id=36f8a21dd2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+            <div id="mc_embed_signup_scroll">
+            <input type="email" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required />
+            <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+              <input type="text" name="b_536179e4b2849e96e841012a4_36f8a21dd2" tabindex="-1" value=""/>
+            </div>
+            <div className="clear">
+              <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
+            </div>
+          </form>
+
+
           {/*
           <Button isSize="large" isColor="info">Get Tickets</Button><br />
           <br />
