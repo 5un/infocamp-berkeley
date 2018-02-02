14c164160e962dc0211027ca1742bd0be14eed8a
Soravis Prakkamakul <asunnotthesun@gmail.com>
2018-02-01 22:52:09 -0800

    Added social icons

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -4,11 +4,35 @@ import { Footer, Columns, Column, Content, Container  } from 'bloomer';
 
 const contactEmail = 'imsa@ischool.berkeley.edu'
 
+const anchorStyle = {  
+  border: '1px solid black', borderRadius: '50px', 
+  width: '70px', height: '70px', 
+  padding: '20px',
+  display: 'inline-block'
+}
+
 const SiteFooter = () => (
   <Footer id="footer">
     <Container hasTextAlign="centered">
       <Content>
         More questions? <a href={`mailto:${contactEmail}`}>Contact us at {contactEmail}</a>
+        <div>
+          <div style={{ display: 'inline-block', margin: '30px' }}>
+            <a href="https://www.facebook.com/InfoCampBerkeley/" style={anchorStyle}>
+              <img src="/images/icons8-facebook-f-96.png" style={{ maxWidth: '24px', maxHeight: '24px' }}/>
+            </a>
+          </div>
+          <div style={{ display: 'inline-block', margin: '30px' }}>
+            <a href="https://twitter.com/infoberkeley/" style={anchorStyle }>
+              <img src="/images/icons8-twitter-96.png" style={{ maxWidth: '24px', maxHeight: '24px' }}/>
+            </a>
+          </div>
+          <div style={{ display: 'inline-block', margin: '30px' }}>
+            <a href="https://www.instagram.com/imsa_berkeley/" style={anchorStyle}>
+              <img src="/images/icons8-instagram-96.png" style={{ maxWidth: '24px', maxHeight: '24px' }}/>
+            </a>
+          </div>
+        </div>
         {/*
         <Columns>
           <Column isFull>
