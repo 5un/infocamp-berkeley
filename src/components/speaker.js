e2be2ffe7c139edfa265a71c2205b3188a7310d9
Soravis Prakkamakul <asunnotthesun@gmail.com>
2018-03-02 22:26:18 -0800

    Updated speaker info

--- src/components/speaker.js
+++ src/components/speaker.js
@@ -4,13 +4,15 @@ import { Column, Image } from 'bloomer'
 
 const Speaker = (props) => (
   <Column isSize="1/3" hasTextAlign="center">
-    <div>
-      <img src={props.speaker.image} style={{ borderRadius: '96px', clip: 'auto', width: '192px', height: '192px' }}/>
-    </div>
-    <div>
-      <a href={props.speaker.link} target="_blank" style={{ textDecoration: 'none' }}><strong>{props.speaker.name}</strong></a><br />
-      <small>{props.speaker.title}</small>
-    </div>
+    <a href={props.speaker.link} target="_blank" style={{ textDecoration: 'none' }}>
+      <div>
+        <img src={props.speaker.image} style={{ borderRadius: '96px', clip: 'auto', width: '160px', height: '160px' }}/>
+      </div>
+      <div>
+        <strong>{props.speaker.name}</strong><br />
+        <small>{props.speaker.title}</small>
+      </div>
+    </a>
   </Column>
 )
 
