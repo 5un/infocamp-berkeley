791b9f66e558e37c4f88e8e911bbe07384a7c003
Soravis Prakkamakul <asunnotthesun@gmail.com>
2018-03-17 13:02:38 -0700

    Put the voting up

--- src/pages/vote.js
+++ src/pages/vote.js
@@ -21,10 +21,18 @@ class IndexPage extends React.Component {
         }} isPaddingless={true} >
         <StickyHeader />
         <Container hasTextAlign="center" style={{ padding: '100px 20px' }}>
-          <h4 style={{ textAlign: 'center' }} >Voting is not yet opened. Stay tuned.</h4>
           {/*
+            <h4 style={{ textAlign: 'center' }} >Voting is not yet opened. Stay tuned.</h4>
+          */}
           <Columns>
             <Column isSize={10} isOffset={1}>
+              <div style={{ textAlign: 'center' }}>
+                <a href="https://docs.google.com/spreadsheets/d/13f4vOnU7JBZnKRBf21u4vlicl2ZgEdFHWkviUy7v-HE/edit?usp=sharing">
+                  <h3>Link to the description of all pitches</h3>
+                </a>
+              </div>
+              <br />
+              <br />
               <Iframe url="https://www.opinionstage.com/polls/2492937/poll"
                 width="100%"
                 height="450px"
@@ -35,7 +43,7 @@ class IndexPage extends React.Component {
                 allowFullScreen/>
             </Column>
           </Columns>
-          */}
+          
         </Container>
       </Section>
     </div>)
