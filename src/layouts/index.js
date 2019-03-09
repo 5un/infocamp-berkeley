19e46468e851150ab13ea7e3f8f0e21207ee21c2
Michael Gutensohn <michael@gutensohn.me>
2019-03-08 20:15:55 -0800

    muti parrot!

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,27 +1,25 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import Link from 'gatsby-link'
 import Helmet from 'react-helmet'
 import { StickyContainer } from 'react-sticky';
-import BurgerMenu from './burger-menu'
-import { Breakpoint } from 'react-responsive-grid'
-import { Footer, Columns, Column, Content, Container } from 'bloomer';
 import './index.scss'
-import Header from './header'
-import SiteFooter from './footer'
+
+import favicon from '../images/favicon.png';
+
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="INFOCAMP Berkeley 2018"
+      title="INFOCAMP Berkeley 2019"
       meta={[
-        { name: 'description', content: 'InfoCamp Berkeley 2018 will be held on March 17, 2018 at South Hall, University of California, Berkeley' },
+        { name: 'description', content: 'InfoCamp Berkeley 2019 will be held on March 17, 2018 at South Hall, University of California, Berkeley' },
         { name: 'keywords', content: 'InfoCamp, uconference, event, information science, datascience, design, society' },
         { name: 'og:url', content: 'https://berkeley-infocamp.org' },
         { name: 'og:type', content: 'website' },
         { name: 'og:title', content: 'INFOCAMP Berkeley 2018' },
         { name: 'og:description', content: 'InfoCamp Berkeley 2018 will be held on March 17, 2018 at South Hall, University of California, Berkeley' },
-        { name: 'og:image', content: 'https://berkeley-infocamp.org/images/fbog-infocamp-2018.png' },
+        link={[
+          { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+      ]}
       ]}
     />
     <StickyContainer>
